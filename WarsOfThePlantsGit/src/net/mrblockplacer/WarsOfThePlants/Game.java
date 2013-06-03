fed818f9ef8b2b8c79fbdfd065e8cdc2ff292778
d4rkfly3r <d4rkfly3r@mrblockplacer.net>
2013-06-03 07:29:54 -0700

    Signed-off-by: d4rkfly3r <d4rkfly3r@mrblockplacer.net>

--- WarsOfThePlantsGit/src/net/mrblockplacer/WarsOfThePlants/Game.java
+++ WarsOfThePlantsGit/src/net/mrblockplacer/WarsOfThePlants/Game.java
@@ -29,6 +29,9 @@ import net.mrblockplacer.WarsOfThePlants.level.TileCoordinate;
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * The width variable for the game
+	 */
 	public static int width = 300;
 	public static int height = (width / 10) * 9;
 	// public static int height = (width / 16) * 9;
